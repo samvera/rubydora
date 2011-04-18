bbd2dfd01c9d745ce124abdad8b00d17b1df04a5
Chris Beer <chris@cbeer.info>
2011-04-18 11:48:51 -0400

    test sparql mapping

---
+++ spec/resource_index_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe Rubydora::ResourceIndex do
+  class MockRepository
+    include Rubydora::ResourceIndex
+  end
+
+  before(:each) do
+    @mock_repository = MockRepository.new
+  end
+
+  it "should map a simple relationship query into SPARQL" do
+    @mock_repository.should_receive(:find_by_sparql) do |query|
+      query.should match(/\<pid\> \<predicate\> \?pid/)
+    end
+
+    @mock_repository.find_by_sparql_relationship('pid', 'predicate')
+  end
+
+  it "should send sparql queries with appropriate parameters" do
+    @mock_risearch = mock()
+    @mock_client = mock(RestClient::Resource)
+    @mock_risearch.should_receive(:post).with(hash_including(:dt => 'on', :format => 'CSV', :lang => 'sparql', :limit => nil, :query => 'placeholder SPARQL query', :type => 'tuples' ))
+    @mock_client.should_receive(:[]).with('risearch').and_return(@mock_risearch)
+    @mock_repository.should_receive(:client).and_return(@mock_client)
+    @mock_repository.send(:risearch, 'placeholder SPARQL query')
+  end
+end
