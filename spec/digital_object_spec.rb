8ad65955c20a50ade42e79590c8905935c406c21
Chris Beer <chris_beer@wgbh.org>
2011-04-19 16:25:17 -0400

    extensions test moved to extension_parameters_spec.rb

--- spec/digital_object_spec.rb
+++ spec/digital_object_spec.rb
@@ -138,47 +138,6 @@ describe Rubydora::DigitalObject do
     end
   end
 
-  describe "extensions" do
-    module FakeExtension
-    end
-
-    module OtherFakeExtension
-
-    end
-    before(:each) do
-      @mock_repository = mock()
-    end
-
-    after(:each) do
-      Rubydora::DigitalObject.registered_extensions = []
-    end
-
-    it "should be extendable" do
-      Rubydora::DigitalObject.use_extension FakeExtension
-      @object = Rubydora::DigitalObject.new 'pid', @mock_repository
-      @object.is_a?(FakeExtension).should == true
-    end
-
-    it "should be extendable conditionally" do
-      Rubydora::DigitalObject.use_extension(FakeExtension) { |x| true }
-      Rubydora::DigitalObject.use_extension(OtherFakeExtension) { |x| false }
-      @object = Rubydora::DigitalObject.new 'pid', @mock_repository
-      @object.is_a?(FakeExtension).should == true
-      @object.is_a?(OtherFakeExtension).should == false
-    end
-
-    it "should be able to introspect object profiles" do
-      @mock_repository.should_receive(:object).any_number_of_times.with({:pid => 'pid'}).and_return <<-XML
-      <objectProfile>
-        <a>1</a>
-      </objectProfile>
-      XML
-      Rubydora::DigitalObject.use_extension(FakeExtension) { |x| x.profile['a'] == '1' }
-      @object = Rubydora::DigitalObject.new 'pid', @mock_repository
-      @object.is_a?(FakeExtension).should == true
-    end
-
-  end
 
   describe "models" do
     before(:each) do
