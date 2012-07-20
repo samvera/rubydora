705ec9ef1a914fdf30141e6623a1a13a381afafc
Chris Beer <cabeer@stanford.edu>
2012-07-20 07:40:29 -0700

    add method-level documentation (and refine api)

--- lib/rubydora/transactions.rb
+++ lib/rubydora/transactions.rb
@@ -1,5 +1,7 @@
 module Rubydora
-  
+  # Extremely basic (and naive) 'transaction' support for Rubydora. This isn't
+  # really intended to be used in a production-like situation -- more for 
+  # rolling back (small) changes during testing.
   module Transactions
     extend ActiveSupport::Concern
 
@@ -61,15 +63,21 @@ module Rubydora
     end
     
 
+    # Start a transaction
     def transaction &block
       Transaction.new self, &block
+      self.transactions_log.clear
     end
 
+    # Unshift a transaction entry onto the transaction logs.
+    # We want these entries in reverse-chronological order
+    # for ease of undoing..
     def append_to_transactions_log *args
       return unless Rubydora::Transactions.use_transactions
       transactions_log.unshift(args)
     end
 
+    # The repository transaction log.
     def transactions_log
       @log ||= []
     end
@@ -79,55 +87,72 @@ module Rubydora
     attr_reader :repository
     def initialize repository, &block
       @repository = repository
-      @old_state = Rubydora::Transactions.use_transactions
-      Rubydora::Transactions.use_transactions = true
-      yield(self)
-      Rubydora::Transactions.use_transactions = @old_state
-      repository.transactions_log.clear
+      with_transactions(&block)
     end
 
-    def rollback
+    def with_transactions &block
       old_state = Rubydora::Transactions.use_transactions
-      Rubydora::Transactions.use_transactions = false 
+      Rubydora::Transactions.use_transactions = true
 
+      yield(self)
+
+      Rubydora::Transactions.use_transactions = old_state
+    end
+
+    def without_transactions &block
+      old_state = Rubydora::Transactions.use_transactions
+      Rubydora::Transactions.use_transactions = false
+
+      yield(self)
+
+      Rubydora::Transactions.use_transactions = old_state
+    end
+
+    # Roll-back transactions by reversing their outcomes 
+    # (or, in some cases, re-ingesting the object at the 
+    # previous state.
+    def rollback
+      without_transactions do
       repository.transactions_log.delete_if do |(method, options)|
 
         begin
-        case method
-          when :ingest
-            repository.purge_object :pid => options[:pid]
+          case method
+            when :ingest
+              repository.purge_object :pid => options[:pid]
 
-          when :modify_object
-            repository.modify_object options
+            when :modify_object
+              repository.modify_object options
 
-          when :add_datastream
-            repository.purge_datastream :pid => options[:pid], :dsid => options[:dsid]
+            when :add_datastream
+              repository.purge_datastream :pid => options[:pid], :dsid => options[:dsid]
 
-          when :add_relationship
-            repository.purge_relationship options[:options].merge(:pid => options[:pid])
+            when :add_relationship
+              repository.purge_relationship options[:options].merge(:pid => options[:pid])
 
-          when :purge_relationship
-            repository.add_relationship options[:options].merge(:pid => options[:pid])
+            when :purge_relationship
+              repository.add_relationship options[:options].merge(:pid => options[:pid])
 
-          when :purge_object
-            repository.ingest :pid => options[:pid], :file => options[:foxml]
+            when :purge_object
+              repository.ingest :pid => options[:pid], :file => options[:foxml]
 
-          when :set_datastream_options
-            repository.set_datastream_options options
+            when :set_datastream_options
+              repository.set_datastream_options options
 
-          when :modify_datastream
-            repository.purge_object :pid => options[:pid] rescue nil
-            repository.ingest :pid => options[:pid], :file => options[:foxml]
+            when :modify_datastream
+              repository.purge_object :pid => options[:pid] rescue nil
+              repository.ingest :pid => options[:pid], :file => options[:foxml]
 
-          when :purge_datastream
-            repository.purge_object :pid => options[:pid] rescue nil
-            repository.ingest :pid => options[:pid], :file => options[:foxml]
-        end
+            when :purge_datastream
+              repository.purge_object :pid => options[:pid] rescue nil
+              repository.ingest :pid => options[:pid], :file => options[:foxml]
+          end
         rescue
+          # no-op
         end
-      true
+
       end
-      Rubydora::Transactions.use_transactions = old_state
+      end
+      true
     end
   end
 
