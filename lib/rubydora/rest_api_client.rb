890aba2af07ef21f9e5b547f57e057db0daff9f7
David Chandek-Stark <dchandekstark@gmail.com>
2017-01-31 15:48:43 -0500

    Patches Rubydora::RestApiClient to not read file-like object
    When adding or modifying datastream content.
    
    Note: Previously, rubydora read the entire file content into a string
    and passed the string to rest-client, so the file remained open.
    Since rest-client closes the file, the changed methods alter the state
    of passed in file/io objects.

--- lib/rubydora/rest_api_client.rb
+++ lib/rubydora/rest_api_client.rb
@@ -336,9 +336,8 @@ module Rubydora
       file = query_options.delete(:content)
       content_type = query_options.delete(:content_type) || query_options[:mimeType] || file_content_type(file)
       run_hook :before_add_datastream, :pid => pid, :dsid => dsid, :file => file, :options => options
-      str = file.respond_to?(:read) ? file.read : file
       file.rewind if file.respond_to?(:rewind)
-      ProfileParser.parse_datastream_profile(client[datastream_url(pid, dsid, query_options)].post(str, :content_type => content_type.to_s, :multipart => true))
+      ProfileParser.parse_datastream_profile(client[datastream_url(pid, dsid, query_options)].post(file, :content_type => content_type.to_s, :multipart => true))
     rescue Exception => exception
       rescue_with_handler(exception) || raise
     end
@@ -361,9 +360,8 @@ module Rubydora
       end
 
       run_hook :before_modify_datastream, :pid => pid, :dsid => dsid, :file => file, :content_type => content_type, :options => options
-      str = file.respond_to?(:read) ? file.read : file
       file.rewind if file.respond_to?(:rewind)
-      ProfileParser.parse_datastream_profile(client[datastream_url(pid, dsid, query_options)].put(str, rest_client_options))
+      ProfileParser.parse_datastream_profile(client[datastream_url(pid, dsid, query_options)].put(file, rest_client_options))
 
     rescue Exception => exception
       rescue_with_handler(exception) || raise
