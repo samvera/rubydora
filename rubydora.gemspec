40ea4e638353855bf89bc7e20b7ee1eefc8f1086
Benjamin Armintor <armintor@gmail.com>
2018-08-14 13:41:57 -0400

    add coveralls configurations and formatter

--- rubydora.gemspec
+++ rubydora.gemspec
@@ -32,4 +32,5 @@ Gem::Specification.new do |s|
   s.add_development_dependency 'jettywrapper', '>= 1.4.0'
   s.add_development_dependency 'webmock'
   s.add_development_dependency 'simplecov'
+  s.add_development_dependency 'coveralls'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,10 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
 require 'simplecov'
+require 'coveralls'
+
+SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+
 SimpleCov.start
 
 require 'rspec/autorun'
